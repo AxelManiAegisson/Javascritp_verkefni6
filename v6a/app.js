cbb517bdebd6053b02d65ed211c0ad997674a432
AxelManiAegisson <33860352+AxelManiAegisson@users.noreply.github.com>
2019-04-07 22:43:22 +0000

    Add files via upload

--- v6a/app.js
+++ v6a/app.js
@@ -1,3 +1,4 @@
+//sæki slider
 var slider = document.getElementById('slider');
 
 noUiSlider.create(slider, {
@@ -7,4 +8,31 @@ noUiSlider.create(slider, {
         'min': 0,
         'max': 100
     }
+    
 });
+//gögn í array
+var people = [
+	{
+		name: 'Axel',
+		skor: 50
+	},
+	{
+		name: 'Palli',
+		skor: 75
+	},
+	{
+		name: 'Kalli',
+		skor: 25
+	},
+	{
+		name: 'Stína',
+		skor: 100
+	},
+		{
+		name: 'Konni',
+		skor: 1
+	}
+];
+
+//filter
+const result = people.filter(skor => word.skor >= slider.range.min && word.skor <= slider.range.max)
