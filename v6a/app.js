731980b264c459b76bf461b2374c0f0274fa670a
AxelManiAegisson <33860352+AxelManiAegisson@users.noreply.github.com>
2019-04-08 01:24:48 +0000

    Add files via upload

--- v6a/app.js
+++ v6a/app.js
@@ -5,10 +5,10 @@ noUiSlider.create(slider, {
     start: [20, 80],
     connect: true,
     range: {
-        'min': 0,
-        'max': 100
+        'min': [0],
+        'max': [100]
     }
-    
+
 });
 //gögn í array
 var people = [
@@ -35,4 +35,44 @@ var people = [
 ];
 
 //filter
-const result = people.filter(skor => word.skor >= slider.range.min && word.skor <= slider.range.max)
+function inRange(value) {
+  
+  return value.skor >= slider.noUiSlider.get()[0] && value.skor <= slider.noUiSlider.get()[1];
+}
+//uppfærir input tagið
+var minCostInput = document.getElementById('value-min'),
+	maxCostInput = document.getElementById('value-max');
+
+slider.noUiSlider.on('update', function( values, handle ) {
+	if ( handle ) {
+		maxCostInput.value = values[handle];
+	} else {
+		minCostInput.value = values[handle];
+	}
+});
+function doSomething(values, handle, unencoded, tap, positions) {
+
+	//birtir gögn sem lista 
+	var result = people.filter(inRange);  
+	var list = document.getElementById("list");
+	while (list.hasChildNodes()) {   
+		list.removeChild(list.firstChild);
+	}
+	document.getElementById('list').appendChild(makeUl(result));
+	}
+
+slider.noUiSlider.on('set', doSomething);
+
+function makeUl(array){	
+	let list = document.createElement('ul');
+	array.forEach(function (i)
+	{		
+		let item = document.createElement('li');
+		
+		item.appendChild(document.createTextNode(i.name));
+
+        list.appendChild(item);
+    });
+
+    return list;
+}
